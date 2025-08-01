e2cbf64d415fc89b5f166a67f317d6c9d7793bb5
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2025-08-01 07:07:33 +0500

    feat: constants file to reuse every variable

---
+++ lib/constants/constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+List<String> banners = [
+  "assets/images/image1.png",
+  "assets/images/image2.png",
+  "assets/images/image3.png",
+];
+List<String> icons = [
+  "assets/icons/icon1.png",
+  "assets/icons/icon2.png",
+  "assets/icons/icon3.png",
+  "assets/icons/icon4.png",
+  "assets/icons/icon5.png",
+];
+List<String> text = ["Beauty", "Baby", "Electronics", "Kitchen", "Medical"];
+
+List<Color> colors = [
+  Colors.orange,
+  Colors.indigo,
+  Colors.green,
+  Colors.lightBlue,
+  Colors.deepOrange,
+];
+
+// products k liye data
+
+List<String> productImages = [
+  "https://sm.ign.com/t/ign_pk/review/a/asus-rog-z/asus-rog-zephyrus-g16-2024-ga605-review_ak6j.1200.jpg",
+  "https://www.cnet.com/a/img/resize/d8e3286904ab60fe0360302e57412ed1b8e10fef/hub/2021/10/22/c94c8e08-3a47-45c4-9ec3-e512ae5f429e/img-6368.jpg?auto=webp&fit=crop&height=675&width=1200",
+  "https://images.samsung.com/pk/smartphones/galaxy-s25-ultra/buy/kv_global_PC_v2.jpg?imbypass=true",
+  "https://m-cdn.phonearena.com/images/hub/274-wide-two_1200/Apple-AirPods-Pro-3-release-date-predictions-price-specs-and-must-know-features.jpg",
+  "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/s10-case-unselect-gallery-1-202503_FMT_WHH?wid=752&hei=720&fmt=p-jpg&qlt=80&.v=T1poMzZuRzBxQ1RzQmhMUHprUE5LZHlVRllKam5abHNZRGludXlMbytKNHlidkR0R3NjTWwrQVNiZ3I2RERPclovcWdSWUQxempSa2dIVnhsNHVhN3ZXdlJRYjdSZWJHVUh4aFVDb0hhVVcwcHBldHBjSVk3TndkU25NUENJakg",
+];
+List<String> productTitles = [
+  "ASUS ROG ZEPHYRUS G16 | RTX 4060",
+  "MacBook M1 Pro Max | 32GB and 1TB (Used) ",
+  "Samsung S25 Ultra | PTA Approved",
+  "Apple AirPods Pro in best price",
+  "Best Apple Watch With Amoled Display",
+];
+
+List<String> productPrices = [
+  "410,999.00",
+  "400,000.00",
+  "439999.00",
+  "19,999.00",
+  "99999.00"
+];
